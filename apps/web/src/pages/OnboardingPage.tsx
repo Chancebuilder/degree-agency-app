977d2969faea48926660f5b50f515727c6ad28fd
Chancebuilder <chancebusinessbuilder@gmail.com>
2026-09-10 12:48:20 -0400

    Fix TypeScript production build errors

--- apps/web/src/pages/OnboardingPage.tsx
+++ apps/web/src/pages/OnboardingPage.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from 'react'
+import type { FormEvent } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../lib/api'
 import { Alert, Button, Card, EstimateNote, Field, Input } from '../ui/primitives'
